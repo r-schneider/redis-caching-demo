f71768ffdfa3157abfee00d32c4a449b88027d8c
Rodrigo Schneider <rmonte.schneider@gmail.com>
2020-02-07 15:18:09 -0300

    feat(cacheservice)project skeleton added

---
+++ cacheservice/src/test/java/com/springredis/cacheservice/CacheserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springredis.cacheservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
